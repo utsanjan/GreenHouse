f731e6f25f64faab50934e574713b4ab8a48f2d7
saptarshi <36296989+Saptarshisamata@users.noreply.github.com>
2020-06-06 14:22:36 +0530

    initial

---
+++ app/src/main/java/com/cemkolaghat/collegeblog/signup/SignUpViewModel.java
@@ -0,0 +1,12 @@
+package com.cemkolaghat.collegeblog.signup;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class SignUpViewModel extends AndroidViewModel {
+    public SignUpViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
